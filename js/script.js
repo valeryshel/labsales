cab94e11447f26feff7f5c65c72543fea30f69bc
valeryshel <valeryshel@gmail.com>
2023-02-22 20:45:52 +0300

    add changes

--- js/script.js
+++ js/script.js
@@ -10,16 +10,6 @@ const swiper = new Swiper(".swiper", {
     el: ".swiper-pagination",
   },
 
-  // Navigation arrows
-  // navigation: {
-  //   nextEl: ".swiper-button-next",
-  //   prevEl: ".swiper-button-prev",
-  // },
-
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: ".swiper-scrollbar",
-  // },
   // Default parameters
   slidesPerView: 1,
   spaceBetween: 10,
