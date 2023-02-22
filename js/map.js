773352dba2aba870971cc2a7f6470a3c72ae7251
valeryshel <valeryshel@gmail.com>
2023-02-22 15:47:29 +0300

    add files and layout

---
+++ js/map.js
@@ -0,0 +1,3 @@
+let placemarkImg = document.querySelector(".ymaps-2-1-79-image ");
+
+console.log(placemarkImg);
---
+++ js/script.js
@@ -0,0 +1,44 @@
+console.log("hello");
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev",
+  // },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: ".swiper-scrollbar",
+  // },
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    // 320: {
+    //   slidesPerView: 2,
+    //   spaceBetween: 10,
+    // },
+    //  when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    // when window width is >= 800px
+    800: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+  },
+});
