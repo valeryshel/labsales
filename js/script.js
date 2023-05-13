3faa81882486bc0c457f7e3b7d36b08387487cdc
valeryshel <valeryshel@gmail.com>
2023-05-13 23:40:31 +0300

    add

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-console.log("hello");
-
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   direction: "horizontal",
@@ -9,6 +7,16 @@ const swiper = new Swiper(".swiper", {
   pagination: {
     el: ".swiper-pagination",
   },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: ".swiper-scrollbar",
+  // },
 
   // Default parameters
   slidesPerView: 1,
@@ -16,18 +24,18 @@ const swiper = new Swiper(".swiper", {
   // Responsive breakpoints
   breakpoints: {
     // when window width is >= 320px
-    // 320: {
-    //   slidesPerView: 2,
-    //   spaceBetween: 10,
-    // },
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
     //  when window width is >= 480px
     480: {
-      slidesPerView: 2,
+      slidesPerView: 1,
       spaceBetween: 10,
     },
     // when window width is >= 800px
     800: {
-      slidesPerView: 4,
+      slidesPerView: 2,
       spaceBetween: 10,
     },
   },
